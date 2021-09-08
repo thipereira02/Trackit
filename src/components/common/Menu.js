281987e9ddb7ef4dda3773cdf5c39ed73c190d25
thipereira02 <thiago.spereira02@gmail.com>
2021-09-08 15:23:18 -0300

    Implementing Menu (with bug)

---
+++ src/components/common/Menu.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
+export default function Menu() {
+	return (
+		<Content>
+			<p>Hábitos</p>
+			<div>
+				<CircularProgressbar 
+					background
+					backgroundPadding={6}
+					value={75}
+					text={"Hoje"}
+					styles={buildStyles({
+						pathColor: "#fff",
+						textColor: "#fff",
+						trailColor: "#52B6FF",
+						backgroundColor: "#52B6FF",
+					})} />
+			</div>
+			<p>Histórico</p>
+		</Content>
+	);
+}
+
+const Content = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 70px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #52B6FF;
+    font-size: 18px;
+    line-height: 22px;
+
+    p:first-child {
+        margin-right: 35px;
+    }
+
+    p:last-child {
+        margin-left: 35px;
+    }
+
+    div {
+        position: absolute;
+        bottom: 20px;
+        left: calc((100vw / 2) - 45.5px);
+        width: 91px;
+        height: 91px;
+        border-radius: 50%;
+        background-color: #52B6FF;
+    }
+`;
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Header from "../components/common/Header";
+import Menu from "../components/common/Menu";
 
 export default function Today() {
 	return (
-		<Header />
+		<>
+			<Header />
+			<Menu />
+		</>
 	);
 }
